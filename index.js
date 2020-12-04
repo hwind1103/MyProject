4c209340a32f48bda3f215ebb1436ee30f0bae47
hwind1103 <skdldnjs2000@gmail.com>
2020-12-04 16:48:07 +0900

    hello

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-변경사항 테스트
+var express = require('express'); // 설치한 express module을 불러와서 변수에 담는다.
+var app = express(); //express를 실행하여 app object를 초기화한다.
+
+app.get('/', function(req, res) { // '/' 위치에 'get'요청을 받는 경우,
+    res.send('Hello World!'); // "Hello World!"를 보냅니다.
+});
+
+var port = 3000; // 사용할 포트 번호를 port 변수에 넣습니다. 
+app.listen(port, function() { // port변수를 이용하여 3000번 포트에 node.js 서버를 연결합니다.
+    console.log('server on! http://localhost:' + port); //서버가 실행되면 콘솔창에 표시될 메세지입니다.
+});
